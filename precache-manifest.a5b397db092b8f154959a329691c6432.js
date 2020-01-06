8bc945abd765f580520ec0f9bc1ae9c43351c9d0
rezaenayati <37056327+rezaenayati@users.noreply.github.com>
2020-01-06 04:23:04 +0330

    Add files via upload

---
+++ precache-manifest.a5b397db092b8f154959a329691c6432.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe99f71d2c979871ac4640f1d63ece87",
+    "url": "/dosir.github.io/index.html"
+  },
+  {
+    "revision": "db362ee4744caa1ec97e",
+    "url": "/dosir.github.io/static/css/2.a212dddb.chunk.css"
+  },
+  {
+    "revision": "41ef28be9c6503471d80",
+    "url": "/dosir.github.io/static/css/main.64c565d3.chunk.css"
+  },
+  {
+    "revision": "db362ee4744caa1ec97e",
+    "url": "/dosir.github.io/static/js/2.294b4391.chunk.js"
+  },
+  {
+    "revision": "d4d38f157f2b11ed564f8e7674c6a850",
+    "url": "/dosir.github.io/static/js/2.294b4391.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41ef28be9c6503471d80",
+    "url": "/dosir.github.io/static/js/main.06aabee8.chunk.js"
+  },
+  {
+    "revision": "b395067f1ee183c4541b",
+    "url": "/dosir.github.io/static/js/runtime-main.b5b4f1b4.js"
+  },
+  {
+    "revision": "e7425dc1f9c856c8253e7b9a39be4d12",
+    "url": "/dosir.github.io/static/media/Vazir-Bold.e7425dc1.ttf"
+  },
+  {
+    "revision": "2c96154baf81b69429f42b7d84a9efa9",
+    "url": "/dosir.github.io/static/media/Vazir.2c96154b.ttf"
+  }
+]);
